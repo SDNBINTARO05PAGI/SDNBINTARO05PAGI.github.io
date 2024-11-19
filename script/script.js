9ffe9d6965c59352cc02c3c3a20b228b1fb34c24
SDNBINTARO05PAGI <kerjapraktekwebsite@gmail.com>
2024-11-19 16:54:05 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+      // Smooth Scroll JavaScript with offset
+      document.querySelectorAll(".nav-links a").forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+          e.preventDefault();
+          const targetId = this.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+          if (targetElement) {
+            const offset = document.querySelector(".top-blue-box").offsetHeight;
+            const elementPosition =
+              targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const offsetPosition = elementPosition - offset;
+            window.scrollTo({
+              top: offsetPosition,
+              behavior: "smooth",
+            });
+          }
+        });
+      });
+
+      // Slideshow JavaScript
+      let slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+        let slides = document.getElementsByClassName("slides");
+        for (let i = 0; i < slides.length; i++) {
+          slides[i].classList.remove("active");
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+          slideIndex = 1;
+        }
+        slides[slideIndex - 1].classList.add("active");
+        setTimeout(showSlides, 3000); // Ganti slide setiap 5 detik
+      }
